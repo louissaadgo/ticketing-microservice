e7201bd53ba4e57a6e04a7b58ac0a0ccded78c92
Louis <louissaadgo@gmail.com>
2020-12-05 14:51:40 +0200

    Fixed some signup bugs

--- auth/routes/signin.go
+++ auth/routes/signin.go
@@ -1,24 +1,9 @@
 package routes
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
-
-	"github.com/louissaadgo/ticketing-microservice/auth/user"
 )
 
 //Signin signs in the user to our app
 func Signin(w http.ResponseWriter, r *http.Request) {
-	credentials := user.Auth{}
-	json.NewDecoder(r.Body).Decode(&credentials)
-	if len(credentials.Email) < 10 {
-		http.Error(w, "Email must be longer than 10 characters", http.StatusBadRequest)
-		return
-	}
-	if len(credentials.Password) < 5 {
-		http.Error(w, "Password must be 5 characters or more", http.StatusBadRequest)
-		return
-	}
-	fmt.Fprintln(w, "Signed up successfully")
 }
