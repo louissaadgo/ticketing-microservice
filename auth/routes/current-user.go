38fa11d86e570c76e3e84c609077b0e5f37d7c45
Louis <louissaadgo@gmail.com>
2020-12-06 23:09:23 +0200

    fixed current user

--- auth/routes/current-user.go
+++ auth/routes/current-user.go
@@ -4,8 +4,5 @@ import "net/http"
 
 //CurrentUser gets the current user signed to our app
 func CurrentUser(w http.ResponseWriter, r *http.Request) {
-	//These are equivalent to http.Error
-	// w.WriteHeader(http.StatusInternalServerError)
-	// w.Write([]byte("500 - Something bad happened!"))
 	http.Error(w, "Invalid email adress", http.StatusForbidden)
 }
