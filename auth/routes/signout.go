bcfeaefd07662163066c5b4d86b548e6a69a7353
Louis <louissaadgo@gmail.com>
2020-12-06 23:08:47 +0200

    Fixed Signout

--- auth/routes/signout.go
+++ auth/routes/signout.go
@@ -4,8 +4,5 @@ import "net/http"
 
 //Signout signs out the user from our app
 func Signout(w http.ResponseWriter, r *http.Request) {
-	//These are equivalent to http.Error
-	// w.WriteHeader(http.StatusInternalServerError)
-	// w.Write([]byte("500 - Something bad happened!"))
 	http.Error(w, "Invalid email adress", http.StatusForbidden)
 }
