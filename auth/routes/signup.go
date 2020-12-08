6e9735b571f69f83914c9c51ebcbcb9fdf8d80ca
Louis <louissaadgo@gmail.com>
2020-12-08 17:47:05 +0200

    Added Password Validation

--- auth/routes/signup.go
+++ auth/routes/signup.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"regexp"
 	"strings"
+	"unicode"
 
 	"github.com/louissaadgo/ticketing-microservice/auth/user"
 )
@@ -27,8 +28,9 @@ func Signup(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Inavlid Email Address", http.StatusBadRequest)
 		return
 	}
-	if len(credentials.Password) < 5 {
-		http.Error(w, "Password must be 5 characters or more", http.StatusBadRequest)
+	//Checks if the password is valid
+	if invalid, errorMsg := isPasswordInvalid(credentials.Password); invalid {
+		http.Error(w, errorMsg, http.StatusBadRequest)
 		return
 	}
 	fmt.Fprintln(w, "Signed up successfully")
@@ -51,4 +53,41 @@ func isEmailInvalid(e string) bool {
 		return true
 	}
 	return false
+}
+
+//Checks if the password is Invalid
+func isPasswordInvalid(s string) (valid bool, errorMsg string) {
+	invalid := false
+	number := false
+	letter := false
+	upperLetter := false
+	specialLetter := false
+	for _, c := range s {
+		switch {
+		case unicode.IsNumber(c):
+			number = true
+		case unicode.IsUpper(c):
+			upperLetter = true
+		case unicode.IsPunct(c) || unicode.IsSymbol(c):
+			specialLetter = true
+		case unicode.IsLetter(c) || c == ' ':
+			letter = true
+		default:
+			invalid = true
+		}
+	}
+	if invalid == true {
+		return true, "Invalid Password Unknown Reason"
+	} else if number == false {
+		return true, "Invalid Password - Password must contain a number"
+	} else if letter == false {
+		return true, "Invalid Password - Password must contain a letter"
+	} else if upperLetter == false {
+		return true, "Invalid Password - Password must contain an uppercase letter"
+	} else if specialLetter == false {
+		return true, "Invalid Password - Password must contain a special letter"
+	} else if len(s) < 7 {
+		return true, "Invalid Password - Password must be at least 7 characters"
+	}
+	return false, ""
 }
