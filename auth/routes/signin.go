50ee2fb011351272bc59f1c3eaf8bca879d55d0c
Louis <louissaadgo@gmail.com>
2020-12-10 19:14:42 +0200

    Added sign up

--- auth/routes/signin.go
+++ auth/routes/signin.go
@@ -1,9 +1,14 @@
 package routes
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"github.com/louissaadgo/ticketing-microservice/auth/user"
 )
 
 //Signin signs in the user to our app
 func Signin(w http.ResponseWriter, r *http.Request) {
+	credentials := user.Model{}
+	json.NewDecoder(r.Body).Decode(&credentials)
 }
