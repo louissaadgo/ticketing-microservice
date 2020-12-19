1a10964177301545447f64c1dc44784306ee3f8e
louissaadgo <louissaadgo@gmail.com>
2020-12-19 20:13:11 +0200

    Fixed multiple errors

--- auth/routes/current-user.go
+++ auth/routes/current-user.go
@@ -1,8 +1,11 @@
 package routes
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 //CurrentUser gets the current user signed to our app
 func CurrentUser(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "Invalid email adress", http.StatusForbidden)
+	fmt.Fprintln(w, "Hi")
 }
