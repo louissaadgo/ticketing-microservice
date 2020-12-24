cce5dc2b2512b0693c5a28128fc340baf1ec072d
louissaadgo <louissaadgo@gmail.com>
2020-12-24 16:03:35 +0200

    got rid of bson

--- auth/user/user-model.go
+++ auth/user/user-model.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 //Model is the user's authentication model
 type Model struct {
-	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Email    string             `json:"email,omitempty" bson:"email,omitempty"`
-	Password string             `json:"password,omitempty" bson:"password,omitempty"`
+	ID       primitive.ObjectID `json:"_id,omitempty"`
+	Email    string             `json:"email,omitempty"`
+	Password string             `json:"password,omitempty"`
 }
