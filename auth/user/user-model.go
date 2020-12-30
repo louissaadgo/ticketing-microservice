ca62de230de36bcd027f0994b53f0c81dbcb4c8a
louissaadgo <louissaadgo@gmail.com>
2020-12-30 15:00:19 +0200

    Fixed User model issue

--- auth/user/user-model.go
+++ auth/user/user-model.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 //Model is the user's authentication model
 type Model struct {
-	ID       primitive.ObjectID `json:"_id" bson:"_id"`
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Email    string             `json:"email"`
 	Password string             `json:"password"`
 }
